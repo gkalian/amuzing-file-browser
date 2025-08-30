05346ac06f3eb0dc1d807e5289f8cde9c54c8138
gkalian <greatkalian@gmail.com>
2025-08-31 02:21:59 +0300

    fix: re-arrange locale files

--- src/client/i18n/index.ts
+++ src/client/i18n/index.ts
@@ -3,8 +3,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import en from './locales/en/common.json';
-import ru from './locales/ru/common.json';
+import en from './locales/en.json';
+import ru from './locales/ru.json';
 
 void i18n
   .use(LanguageDetector)
