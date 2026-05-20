80c31db8f73031704308be584796238446736106
gkalian <greatkalian@gmail.com>
2026-05-20 15:56:22 +0300

    fix: libs update and chunk warning removed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['react', 'react-dom', 'use-callback-ref'],
+    include: [
+      'react',
+      'react-dom',
+      'use-callback-ref',
+      '@mantine/core',
+      '@mantine/hooks',
+      '@mantine/notifications',
+      'i18next',
+      'react-i18next',
+      'i18next-browser-languagedetector',
+      'react-virtuoso',
+    ],
     needsInterop: ['use-callback-ref'],
   },
   resolve: {
@@ -26,5 +37,30 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'vendor-react';
+          }
+          if (id.includes('node_modules/@mantine')) {
+            return 'vendor-mantine';
+          }
+          if (id.includes('node_modules/@tabler')) {
+            return 'vendor-tabler';
+          }
+          if (
+            id.includes('node_modules/i18next') ||
+            id.includes('node_modules/react-i18next') ||
+            id.includes('node_modules/i18next-browser-languagedetector')
+          ) {
+            return 'vendor-i18n';
+          }
+          if (id.includes('node_modules/react-virtuoso')) {
+            return 'vendor-virtuoso';
+          }
+        },
+      },
+    },
   },
 });
